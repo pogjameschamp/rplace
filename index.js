9d044c9b844912a92bfb8f648e810d36c85a0a43
James Le Schultz <jamesleschultz@Jamess-MacBook-Pro.local>
2023-10-20 02:11:17 +1100

    pog

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express'),
-      app = express(),
-      server = require('http').createServer(app),
-      io = require('socket.io')(server),
-      MongoClient = require('mongodb').MongoClient;
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io')(server),
+MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://pogjameschamp:pogjameschamp@cluster0.vgagxdh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(express.static("public"));
 
 const CANVAS_ROWS = 100;
 const CANVAS_COLS = 100;
-
+let pixelCollection;
 var canvas = [];
+
 for(var row = 0; row < CANVAS_ROWS; row++){
     canvas[row] = [];
     for(var col = 0; col < CANVAS_COLS; col++){
@@ -15,18 +19,9 @@ for(var row = 0; row < CANVAS_ROWS; row++){
     }
 }
 
-app.use(express.static("public"));
-
-const uri = "mongodb+srv://pogjameschamp:pogjameschamp@cluster0.vgagxdh.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-let pixelCollection;
-
 client.connect()
     .then(() => {
         console.log("Connected to MongoDB");
-
-
     // retreieve pixel data and populate grid
     const database = client.db("placeDB");
     pixelCollection = database.collection("pixels");
@@ -35,24 +30,24 @@ client.connect()
     return pixelCollection.find({}).toArray();
 })
 .then(pixels => {
-    console.log("PIXELS BEING RETRIEVED:", pixels.length);
+    // populate canvas with pixel data.
     pixels.forEach(pixel => {
         canvas[pixel.row][pixel.col] = pixel.color;
     });
   
-
-
     // start the server and listen for socket connections AFTER loading pixels
     io.on("connection", socket => {
       console.log('User connected');
-      
       socket.emit("canvas", canvas);
-  
+
+      // On receiving color and row+col from frontend add info to database and emit the new canvas.
       socket.on("color", data => {
           if(data.row < CANVAS_ROWS && data.row >= 0 && data.col < CANVAS_COLS && data.col >= 0){
               canvas[data.row][data.col] = data.color;
+              // emit new canvas to frontend. 
               io.emit("canvas", canvas);
   
+              // update data in database
               pixelCollection.updateOne(
                   { row: data.row, col: data.col },
                   { $set: { color: data.color } },
--- public/js/place.js
+++ public/js/place.js
@@ -1,30 +1,36 @@
 $(document).ready(() => {
   var socket = io()
-
   var canvas = $("#place")[0]
   var ctx = canvas.getContext("2d")
-
   let canvasData = [];  // Store this so we can reference it for hover redraws
   canvas.width = 1000;
   canvas.height = 1000;
-  
   let currentColor = 'black';
-
-
   const PIXEL_SIZE = canvas.width / 100;
-
   let hoveredPixel = {col: null, row: null};  // Track the currently hovered pixel
 
-  $('.color-btn').click(function() {
-    currentColor = $(this).data('color');
-});
+    // retrieve canvas data for pixel placement and draw the pixels.
+    socket.on("canvas", data => {
+        canvasData = data;
+        data.forEach((row, rowIndex) => {
+            row.forEach((col, colIndex) => {
+                drawPixel(colIndex, rowIndex, col);
+            })
+        })
+    });
+
+    // change the current color to the color user presses
+    $('.color-btn').click(function() {
+        currentColor = $(this).data('color');
+    });
 
   // Redraw a specific pixel based on its data
-  function drawPixel(col, row, color) {
-      ctx.fillStyle = color;
-      ctx.fillRect(col * PIXEL_SIZE, row * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
-  }
+    function drawPixel(col, row, color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(col * PIXEL_SIZE, row * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+    }
 
+    // Change color of pixel that mouse hovers above to light grey.
   canvas.addEventListener('mousemove', (event) => {
       const rect = canvas.getBoundingClientRect();
       const x = event.clientX - rect.left;
@@ -32,6 +38,7 @@ $(document).ready(() => {
 
       const col = Math.floor(x / PIXEL_SIZE);
       const row = Math.floor(y / PIXEL_SIZE);
+
 
       if (hoveredPixel.col !== col || hoveredPixel.row !== row) {
           if (hoveredPixel.col !== null && hoveredPixel.row !== null) {
@@ -46,6 +53,8 @@ $(document).ready(() => {
       }
   });
 
+
+  // On click event send the color and row to server.
   canvas.addEventListener('click', (event) => {
       const rect = canvas.getBoundingClientRect();
       const x = event.clientX - rect.left;
@@ -54,20 +63,10 @@ $(document).ready(() => {
       const col = Math.floor(x / PIXEL_SIZE);
       const row = Math.floor(y / PIXEL_SIZE);
 
-      const color = $("#color").val();
       socket.emit("color", {
           col: col,
           row: row,
           color: currentColor
       });
-  });
-
-  socket.on("canvas", data => {
-      canvasData = data;
-      data.forEach((row, rowIndex) => {
-          row.forEach((col, colIndex) => {
-              drawPixel(colIndex, rowIndex, col);
-          })
-      })
   });
 });
